eef9b53c376a00cdbfa70a29988d4cc1c598c52b
Stephen Lindberg <sllindberg21@students.tntech.edu>
2012-08-15 15:36:39 -0400

    Implemented JageCamera class. This provides a pannable, zoomable, rotatable camera affine transform.

--- jage2D/jage2Dsrc/Camera.js
+++ jage2D/jage2Dsrc/Camera.js
@@ -12,29 +12,174 @@
  * JageCamera
  * Used to create a 2D camera affine transform and provide functions for 
  * converting back and forth from world to canvas coordinates with this transform.
+ *
+ * The camera will start with no tilt nor zoom with its focus in the center of the 
+ * parent canvas at world coordinates 0,0.
+ *
  * Inputs: painter is a CanvasRenderingContext2D.
+ * Depends on: JageMath.js
  * */
 
 function JageCamera(painter) {
-	// Require that the painter is a CanvasRenderingContext2D.
-	if(painter instanceof CanvasRenderingContext2D) {
-        this.painter = painter;
+    // DATA
     
+    /** A reference to the Canvas's rendering context. */
+    this.painter = painter;
+
+    /** The width and height of the parent Canvas. */
+    this.width = painter.canvas.width;
+    this.height = painter.canvas.height;
+
+    /** The x,y position of the camera's focal center in world coordinates */
+    this.x = 0.0;
+    this.y = 0.0;
+
+    /** The current x,y position of the camera's focal center in screen coordinates. */
+    this.cx = this.width/2.0;
+    this.cy = this.height/2.0;
+
+    /** Controls the camera's zoom. This can be any number in the range (0,+Infinity). 
+    It is zoomed in closer to +Infinity and zoomed out closer to 0. */
+    this.zoom = 1.0;
+
+    /** The camera's tilt angle in degrees. */
+    this.angle = 0.0;
+
+    /** The position of the parent Canvas's edges in world coordinates. (These values are only accurate when this.angle is 0) */
+    this.xLeft = 0-this.cx;
+    this.xRight = this.cx;
+    this.yTop = 0-this.cy;
+    this.yBottom = this.cy;
+    
+    /** The camera's current affine transform and its inverse. */
+    this.trans = null;
+    this.inv = null;
+    
+    // METHODS
+    
+    /** Resets the camera's to its default state. */
+	this.reset = function () {
+        this.width = painter.canvas.width;
+        this.height = painter.canvas.height;
         this.x = 0.0;
         this.y = 0.0;
-        this.cx = 0.0;
-        this.cy = 0.0;
+        this.cx = this.width/2.0;
+        this.cy = this.height/2.0;
         this.zoom = 1.0;
         this.angle = 0.0;
-		this.width = painter.canvas.width;
-		this.height = painter.canvas.height;
-		
-		
-	}
-	else {
-		alert("Caution: This camera doesn't have a graphics context.");
-	}
-	
+        this.xLeft = 0-this.cx;
+        this.xRight = this.cx;
+        this.yTop = 0-this.cy;
+        this.yBottom = this.cy;
+        this.updateTransform();
+    }
+    
+    /** Updates the Camera's transform using its current state values. */
+    this.updateTransform = function () {
+        // compute the camera's affine transform
+        this.trans = JageAffTrans.id();
+        this.trans.translate(this.cx, this.cy);
+        this.trans.scale(this.zoom, this.zoom);
+        this.trans.rotate(this.angle);
+        this.trans.translate(0-this.x, 0-this.y);
+        
+        // compute the transform's inverse
+        this.inv = this.trans.inv();
+        
+        // compute the world coordinates of the parent Canvas's 
+        // upperleft and lower right corners.
+        var ul = this.inv.apply(new JagePoint(0,0));
+        var lr = this.inv.apply(new JagePoint(this.width, this.height));
+        
+        // compute the world positions of the parent Canvas's edges.
+        this.xLeft = ul.x;
+        this.xRight = lr.x;
+        this.yTop = ul.y;
+        this.yBottom = lr.y;
+    }
+    
+    
+    /** Returns a point transformed from screen to world coordinates. 
+    The original point is unchanged. */
+    this.screenToWorld = function (pt) {
+        return this.inv.apply(pt);
+    }
+    
+    this.s2w = this.screenToWorld
+    
+    /** Returns a point transformed from world to screen coordinates.
+    The original point is unchanged. */
+    this.worldToScreen = function (pt) {
+        return this.trans.apply(pt);
+    }
+    
+    
+    /** Moves the camer's focal center without altering the current camera transform. */
+    this.moveCenter2 = function (cx, cy) {
+        var dcx = cx - this.cx;
+        var dcy = cy - this.cy;
+        
+        // move the focal center.
+        this.cx = cx;
+        this.cy = cy;
+        
+        // change the focal center's world position without 
+        // altering the camera's current tranform.
+        var dc = new JagePoint(dcx,dcy);
+        var sr = JageAffTrans.id().scale(this.zoom, this.zoom).rotate(this.angle);
+        var srInv = sr.inv();
+        dc = srInv.apply(dc);
+        
+        this.x += dc.x;
+        this.y += dc.y;
+        
+        this.updateTransform();
+    }
+    
+    /** Moves the camer's focal center without altering the current camera transform. */
+    this.moveCenter = function(screenPt) {
+        this.moveCenter2(screenPt.x, screenPt.y);
+    }
+    
+    /** Used to move the camera by dragging it with the mouse or some other object with screen coordinates. */
+    this.drag = function(screenPt) {
+        if(this.dragTimer == 0) {
+            // On our first drag iteration, obtain the anchor point for the drag, the focus's starting world coords, and our starting inverse.
+            this.dragAnchor = s2w(screenPt);
+            this.dragStartPt = new JagePoint(this.x, this.y);
+            this.dragInv = this.inv.clone();
+        }
+        else {
+            // On subsequent drag iterations, update the camera focal point's screen and world coordinates relative to the anchor point.
+            var worldPt = this.dragInv.apply(screenPt);
+            var vector = new JagePoint(this.dragAnchor.x - worldPt.x, this.dragAnchor.y - worldPt.y);
+            
+            this.x = dragStartPt.x + vector.x;
+            this.y = dragStartPt.y + vector.y;
+            
+            this.moveCenter(screenPt);
+        }
+        this.dragTimer++;
+    }
+    
+    
+    /** Zooms in (or out) on a point in screen coordinates. */
+    this.zoomAtScr = function (zoomMult, screenPt) {
+        this.moveCenter(screenPt);
+        this.zoom *= zoomMult;
+        
+        this.updateTransform();
+    }
+    
+    /** Zooms in (or out) on a point in world coordinates. */
+    this.zoomAtWor = function (zoomMult, worldPt) {
+        var screenPt = this.trans.apply(worldPt);
+        this.zoomAtScr(zoomMult, screenPt);
+    }
+    
+    // initialize Camera's affine transform from its initial values.
+    this.updateTransform();
+    this.dragTimer = 0;
 }
 
 
--- jage2D/main.js
+++ jage2D/main.js
@@ -9,11 +9,8 @@ $(document).ready(function () {
 	// obtain our graphics context from the canvas object.
 	var c = document.getElementById("canvas");
 	var gfx = c.getContext("2d");
-    var degrees = prompt("Input an angle as degrees: ", "");
-    alert("you said " + degrees)
-    alert("In radians: " + JageMath.d2r(degrees));
-    alert("cos: " + JageMath.cos(degrees));
-    alert("sin: " + JageMath.sin(degrees));
+    
+    
 
 });
 
